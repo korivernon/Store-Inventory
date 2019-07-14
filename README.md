a687e97c64173ade82542f0e3d819d6b073993b3
korivernon <44047473+korivernon@users.noreply.github.com>
2019-07-14 04:09:59 -0400

    latest

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Store-Inventory
 Store Inventory Manager Using SQL and Python
+
+SQLAlchemy
+
+By using SQLAlchemy in our Python code, all records can be obtained with a line like contacts = Contact.query.all() instead of a plain SQL such as SELECT * FROM contacts. That may not look like much of a difference in syntax but writing the queries in Python is often faster and easier for many Python developers once multiple tables and specific filtering on fields for queries have to be written.
--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, flash, redirect, jsonify, render_template, request, session, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+import mysql.connector
+from datetime import datetime
 # from app import db
 import os
 
@@ -8,45 +10,51 @@ import os
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 # Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'db.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:R0bin54!@localhost/storeinv?auth_plugin=mysql_native_password'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# Initialize
+# Initialize Database
 db = SQLAlchemy(app)
 
 # Initialize Marshmallow
 ma = Marshmallow(app)
 
-#items = ['T-Shirt','Pants','Ice Cream']
-
 # Product Class/Model
 # Model gives predefined methods
 class Product(db.Model):
     # var_name = db.Column((database).TYPE)
+    __tablename__ = 'product'
     id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(100),unique=True)
-    description = db.Column(db.String(200))
-    price = db.Column(db.Float)
-    inStock = db.Column(db.Boolean)
+    name = db.Column(db.String(100),unique=True, nullable = False)
+    image_file = db.Column(db.String(20),unique=True,nullable=False)
+    description = db.Column(db.String(200), nullable = False)
+    price = db.Column(db.Float, nullable = False )
+    instock = db.Column(db.Boolean, nullable = False)
+    date_posted = db.Column(db.DateTime,nullable=False, default=datetime.utcnow)
 
-    def __init__(self, name, description, price, inStock):
+    def __init__(self, name, description, price, instock, image_file, date_posted):
         self.name = name
         self.description = description
         self.price = price
-        if inStock == True:
-            self.inStock == "In Stock"
+        if instock == True:
+            self.instock == "In Stock"
         else:
-            self.inStock == "Out of Stock"
+            self.instock == "Out of Stock"
+        self.image_file = image_file
+        self.date_posted = date_posted
+
+    # How the information will be displayed
+    def __repr__(self):
+        return f"Product('{self.name}','{self.image_file}','{self.description}','{self.price}','{self.instock}')"
 
 # Product Schema
 # The output to the front end - this is what we want to show.
 class ProductSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'name', 'description','price', 'inStock')
+        fields = ('id', 'name', 'description','price', 'instock')
 
 # Init Schema
 product_schema = ProductSchema(strict = True)
-products_schema = ProductSchema( many = True , string=True)
+products_schema = ProductSchema( many = True , strict=True)
 
 # Create a Product
 @app.route('/product',methods=['POST'])
@@ -54,9 +62,9 @@ def add_product():
     name = request.json['name']
     description = request.json['description']
     price = request.json['price']
-    inStock = request.json['inStock']
+    instock = request.json['instock']
 
-    new_product = Product(name,description,price,inStock)
+    new_product = Product(name,description,price,instock)
 
     # Add to the Databse
     db.session.add(new_product)
@@ -79,25 +87,25 @@ def get_product(id):
     return jsonify(result.data)
 
 # UPDATE THE PRODUCT
-@app.route('/product/<id>',method=['PUT'])
+@app.route('/product/<id>',methods=['PUT'])
 def update_product(id):
     product = Product.query.get(id)
 
     name = request.json['name']
     description = request.json['description']
     price = request.json['price']
-    inStock = request.json['inStock']
+    instock = request.json['instock']
 
 
     product.name = name
     product.description = description
     product.price = price
-    product.inStock = inStock
+    product.instock = instock
 
     db.session.commit()
-    return product_schema.jsonify(product
+    return product_schema.jsonify(product)
 
-@app.route('/product.<id>',method=['DELETE'])
+@app.route('/product.<id>',methods=['DELETE'])
 def delete_product(id):
     product = Product.query.get(id)
     db.session.delete(product)
@@ -105,22 +113,6 @@ def delete_product(id):
     db.session.commit()
     return product_schema.jsonify(product)
 
-'''
-@app.route("/catalog/")
-def catalog():
-    # This displays the items by setting the items equal to their respective parts.
-    # We want to get the price of the items.
-    # We want to get the status of the items.
-    # We want to display these items in conjunction with one another.
-    return render_template(
-    'catalog.html',
-    items=items)
-
-@app.route("/item/<user>/")
-def item(user):
-    return render_template(
-    'output.html',item_num=user, clothing_article = items[int(user)-1])
-'''
 # Run Server
 if __name__ == "__main__":
     db.create_all()
